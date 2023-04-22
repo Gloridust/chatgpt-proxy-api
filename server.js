17b8b0736b7156c40c54d0eccdf25303f5679fb2
Gloridust <Gloridust@gmail.com>
2023-04-22 14:24:41 +0800

    start

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.use('/chatgpt', createProxyMiddleware({ target: 'https://api.openai.com/v1/', changeOrigin: true }));
+
+app.listen(3000, () => {
+  console.log('Proxy server listening on port 3000');
+});
