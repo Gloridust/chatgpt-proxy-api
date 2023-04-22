091255bd84c4c5e84002e03ed35a2badba861cbf
Gloridust <Gloridust@gmail.com>
2023-04-22 14:42:11 +0800

    change port

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const app = express();
 app.use('/chatgpt', createProxyMiddleware({ target: 'https://api.openai.com/v1/', changeOrigin: true }));
 
 app.listen(3000, () => {
-  console.log('Proxy server listening on port 3000');
+  console.log('Proxy server listening on port 3090');
 });
